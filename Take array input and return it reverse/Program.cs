9b52378fc8a652eb5361ceecfab676dc91f06e39
NicholasKessler <36415001+NicholasKessler@users.noreply.github.com>
2018-02-17 12:22:53 -0800

    Update Program.cs

--- Take array input and return it reverse/Program.cs
+++ Take array input and return it reverse/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace Take_array_input_and_return_it_reverse
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter the numbers you wish to store");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Number {0} = ", i + 1);
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine("{0}", numbers[i]);
+            }
         }
     }
 }
